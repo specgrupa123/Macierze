3adfd0dbb5674be8d46696964e1e720813713386
root <root@pawel-Satellite-A500>
2016-01-16 14:19:37 +0100

    test

---
+++ src/macierze/Macierze.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package macierze;
+
+import methods.Matrix_Add;
+
+/**
+ *
+ * @author root
+ */
+public class Macierze {
+
+    public static void main(String[] args) {
+        Matrix_Add mCreate = new Matrix_Add();
+    }
+
+}
---
+++ src/methods/Matrix_Add.java
@@ -0,0 +1,92 @@
+package methods;
+
+import java.util.Scanner;
+
+public class Matrix_Add {
+
+    Scanner scan;
+    int matrix1[][], matrix2[][], sum[][];
+    int row, column;
+
+    public void create() {
+
+        scan = new Scanner(System.in);
+
+        System.out.println("Dodawanie macierzy");
+
+        System.out.println("\nWprowadz liczbe wierszy i kolumn");
+        row = Integer.parseInt(scan.nextLine());
+        column = Integer.parseInt(scan.nextLine());
+
+        matrix1 = new int[row][column];
+        matrix2 = new int[row][column];
+        sum = new int[row][column];
+
+        System.out.println("Wprowadz pierwsza macierz");
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < column; j++) {
+
+                matrix1[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Wprowadz druga macierz");
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < column; j++) {
+
+                matrix2[i][j] = scan.nextInt();
+            }
+        }
+    }
+
+    public void display() {
+
+        System.out.println("\nPierwsza macierz");
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < column; j++) {
+
+                System.out.print("\t" + matrix1[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\nDruga macierz");
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < column; j++) {
+
+                System.out.print("\t" + matrix2[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void add() {
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < column; j++) {
+
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        System.out.println("\n\npo dodaniu macierzy: ");
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < column; j++) {
+
+                System.out.print("\t" + sum[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
